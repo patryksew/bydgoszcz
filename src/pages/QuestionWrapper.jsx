c78fd856a002127b0d41c691b5c7fd7a692dfd8c
Patryk Sewastianowicz <patryksewastianowicz@gmail.com>
2025-12-06 20:20:33 +0100

    Revert "Remove LanguageSelector from QuestionWrapper and TrailsWrapper components"
    This reverts commit 9e2d3782b3a69562d296c66b5d6d81fa89292ff0.

--- src/pages/QuestionWrapper.jsx
+++ src/pages/QuestionWrapper.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import { useLanguage } from "../hooks/useLanguage";
+import LanguageSelector from "../components/LanguageSelector";
 import Question from "./Question";
 
 export default function QuestionWrapper() {
-  const { lang } = useLanguage();
+  const { lang, setLang } = useLanguage();
   const [points, setPoints] = useState(0);
 
+  if (!lang) {
+    return <LanguageSelector onSelect={setLang} />;
+  }
 
   return <Question points={points} setPoints={setPoints} lang={lang} />;
 }
--- src/pages/TrailsWrapper.jsx
+++ src/pages/TrailsWrapper.jsx
@@ -1,8 +1,13 @@
 import { useLanguage } from "../hooks/useLanguage";
+import LanguageSelector from "../components/LanguageSelector";
 import Trails from "./Trails";
 
 export default function TrailsWrapper() {
-  const { lang } = useLanguage();
+  const { lang, setLang } = useLanguage();
+
+  if (!lang) {
+    return <LanguageSelector onSelect={setLang} />;
+  }
 
   return <Trails lang={lang} />;
 }
