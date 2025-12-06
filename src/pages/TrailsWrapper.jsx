9e2d3782b3a69562d296c66b5d6d81fa89292ff0
Patryk Sewastianowicz <patryksewastianowicz@gmail.com>
2025-12-06 20:12:40 +0100

    Remove LanguageSelector from QuestionWrapper and TrailsWrapper components

--- src/pages/TrailsWrapper.jsx
+++ src/pages/TrailsWrapper.jsx
@@ -1,13 +1,8 @@
 import { useLanguage } from "../hooks/useLanguage";
-import LanguageSelector from "../components/LanguageSelector";
 import Trails from "./Trails";
 
 export default function TrailsWrapper() {
-  const { lang, setLang } = useLanguage();
-
-  if (!lang) {
-    return <LanguageSelector onSelect={setLang} />;
-  }
+  const { lang } = useLanguage();
 
   return <Trails lang={lang} />;
 }
